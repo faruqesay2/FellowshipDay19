dcfea45597bd054f5f5ccacb66ca85088e482dcb
FARUQESAY <113195467+faruqesay2@users.noreply.github.com>
2022-11-22 20:28:58 +0530

    UC_2

---
+++ RegexPrograms/RegexPrograms/ValidLastName.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    public class UserValidation
+    {
+        public const string FIRST_NAME_REGEX = "^[A-Z][a-zA-Z]{2}$";
+        public void ValidationFirstName(string firstname)
+        {
+            Regex regex = new Regex(FIRST_NAME_REGEX);
+            bool result = regex.IsMatch(firstname);
+            Console.WriteLine(result);
+        }
+        
+        //LastName the First Letter Should Capital and minimum 3 Character
+        public const string LASTNAME_REGEX = "^[A-Z][a-zA-Z]{2}$";
+        public void ValidationLastName(string lastname)
+        {
+            Regex regex = new Regex(LASTNAME_REGEX);
+            bool result = regex.IsMatch(lastname);
+            Console.WriteLine(result);
+        }
+    }
+}
