f10ffff178fbeb240e95acf3e56d569b18626667
FARUQESAY <113195467+faruqesay2@users.noreply.github.com>
2022-11-22 20:37:42 +0530

    UC_7

---
+++ RegexPrograms/RegexPrograms/AtLeastOneCharacterNumeric.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    public class UserValidation
+    {
+        public const string FIRST_NAME_REGEX = "^[A-Z][a-zA-Z]{2}$";
+        public void ValidationFirstName(string firstname)
+        {
+            Regex regex = new Regex(FIRST_NAME_REGEX);
+            bool result = regex.IsMatch(firstname);
+            Console.WriteLine(result);
+        }
+        //LastName the First Letter Should Capital and minimum 3 Character
+        public const string LASTNAME_REGEX = "^[A-Z][a-zA-Z]{2}$";
+        public void ValidationLastName(string lastname)
+        {
+            Regex regex = new Regex(LASTNAME_REGEX);
+            bool result = regex.IsMatch(lastname);
+            Console.WriteLine(result);
+        }
+        //E-Mail validation
+        public const string EMAIL_REGEX = "^[a-z0-9]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}$";
+        public void ValidationEmail(string email)
+        {
+            Regex regex = new Regex(EMAIL_REGEX);
+            bool result = regex.IsMatch(email);
+            Console.WriteLine(result);
+        }
+        // Pre define Mobile Format Validation
+        public const string MOBILENUMBER_REGEX = "^[0-9]+[\\s]+[0-9]{10}$";
+        public void ValidationPhoneNumber(string phonenumber)
+        {
+            Regex regex = new Regex(MOBILENUMBER_REGEX);
+            bool result = regex.IsMatch(phonenumber);
+            Console.WriteLine(result);
+        }
+        public const string PASSWORD_REGEX = "[a-z,A-Z,0-9]{8,}$";
+        public void ValidationPassword(string password)
+        {
+            Regex regex = new Regex(PASSWORD_REGEX);
+            bool result = regex.IsMatch(password);
+            Console.WriteLine(result);
+        }
+        public const string UPPERCASE_REGEX = "^[A-Z][a-z]{7}$";
+        public void ValidationUppercase(string Uppercase)
+        {
+            Regex regex = new Regex(UPPERCASE_REGEX);
+            bool result = regex.IsMatch(Uppercase);
+            Console.WriteLine(result);
+        }
+        public const string NUMERICPASSWORD_REGEX = "^[a-z0-9A-Z]{8}$";
+        public void ValidationNumericPassword(string numericpassword)
+        {
+            Regex regex = new Regex(NUMERICPASSWORD_REGEX);
+            bool result = regex.IsMatch(numericpassword);
+            Console.WriteLine(result);
+        }
+    }
+}
